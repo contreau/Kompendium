5b563568f6a329a8ba06cb074d1a31669b832f51
contreau <72581161+contreau@users.noreply.github.com>
2024-11-19 15:06:15 -0500

    optimized json, removed redundancies

--- src/lib/server/processJSON.ts
+++ src/lib/server/processJSON.ts
@@ -1,7 +1,8 @@
-import type { playlistJSON } from '$lib/types';
+import type { playlistJSON } from "$lib/types";
 
 const modules: Record<string, { default: playlistJSON }> = import.meta.glob(
-	'/static/playlists.json',
-	{ eager: true }
+  "/static/playlists-fmt.json",
+  { eager: true }
 );
-export const fullPlaylistJSON: playlistJSON = modules['/static/playlists.json']['default'];
+export const fullPlaylistJSON: playlistJSON =
+  modules["/static/playlists-fmt.json"]["default"];
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,13 @@
-import type { playlistJSON } from '$lib/types';
-import { formatBasePlaylists } from '$lib/functions';
+import type { playlistJSON } from "$lib/types";
 
 // TODO: add a jump to top button after x amount of scrolling
 
 export const load = async () => {
-	const { fullPlaylistJSON } = await import('$lib/server/processJSON');
-	let playlists: playlistJSON = formatBasePlaylists(fullPlaylistJSON);
-	playlists = playlists.slice(0, 100);
+  const { fullPlaylistJSON } = await import("$lib/server/processJSON");
+  let playlists: playlistJSON = fullPlaylistJSON;
+  playlists = playlists.slice(0, 100);
 
-	return {
-		playlists
-	};
+  return {
+    playlists,
+  };
 };
