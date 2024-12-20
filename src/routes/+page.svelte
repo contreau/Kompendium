685fc2aaeb4463b8d15a49312e8811e512e6e4f4
contreau <72581161+contreau@users.noreply.github.com>
2024-12-20 01:34:29 -0500

    landing page dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import Nav from "$lib/components/Nav.svelte";
-  import MobileNav from "$lib/components/MobileNav.svelte";
-  import SpotifySVG from "$lib/svg/SpotifySVG.svelte";
-
   import { onMount } from "svelte";
 
   onMount(() => {
@@ -36,55 +32,116 @@
 </script>
 
 <svelte:head>
-  <title>Konrad's Songbook</title>
+  <title>Kompendium • In Memory of Konrad Rauscher</title>
 </svelte:head>
 
 <header>
-  <div>
-    <h1>Konrad's Songbook</h1>
-    <p class="stats">
-      <span class="stats-item"><span>826</span> Original Playlists</span>
-      <span class="stats-item separator">•</span>
-      <span class="stats-item">
-        <span>59,903</span>
-        Songs
-      </span>
-      <span class="stats-item separator">•</span>
-      <a
-        href="https://open.spotify.com/user/1261690341?si=235dbc5649f8441e"
-        target="_blank"><SpotifySVG /></a
-      >
-      <span class="separator">•</span>
-      <a href="/in-memoriam">In Memoriam</a>
-    </p>
-  </div>
+  <h1>Kompendium</h1>
+  <p class="stats">
+    <span class="stats-item"
+      >By <a
+        href="https://open.spotify.com/user/1261690341?si=d3127b482cc14c4b"
+        target="_blank">Konrad Rauscher</a
+      ></span
+    >
+    <span class="stats-item separator">•</span>
+    <span class="stats-item"><span>11</span> Years</span>
+    <span class="stats-item separator">•</span>
+    <span class="stats-item"><span>826</span> Original Playlists</span>
+    <span class="stats-item separator">•</span>
+    <span class="stats-item"><span>59,903</span> Songs</span>
+  </p>
 </header>
 
-<main>
-  <div class="wave-container" id="waveContainer">
-    {#each { length: 25 } as _}
-      <div class="wave-bar"></div>
-    {/each}
-  </div>
-  <MobileNav rangeLabel="" />
-  <Nav rangeLabel="" />
-</main>
+<nav class="site-links">
+  <p><a href="/playlists/1-100">Explore Playlists</a></p>
+  <p><a href="/in-memoriam">In Memoriam</a></p>
+</nav>
+
+<!-- WAVE VISUAL -->
+<section class="wave-container" id="waveContainer">
+  {#each { length: 50 } as _}
+    <div class="wave-bar"></div>
+  {/each}
+</section>
 
 <style>
+  header {
+    border-bottom: solid 2px #6290fc18;
+  }
+
+  nav {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin-top: 5.5rem;
+    margin-bottom: 5.5rem;
+
+    p {
+      text-align: center;
+      font-size: 1.8rem;
+      font-weight: 500;
+      letter-spacing: 0.5px;
+    }
+
+    a {
+      text-decoration: none;
+      color: var(--text);
+      padding: 0.5em;
+      border: solid 1.5px transparent;
+      border-radius: 10px;
+      will-change: filter;
+      filter: drop-shadow(0 0 0.45rem var(--text));
+      transition: all 0.2s;
+      &:hover {
+        color: #467eff;
+        filter: drop-shadow(0 0 0.45rem #467eff);
+      }
+    }
+  }
+
+  h1 {
+    font-size: clamp(2.5rem, 10vw, 6.5rem);
+    font-weight: 600;
+    margin-top: 0.25rem;
+    margin-bottom: 0.25rem;
+    letter-spacing: 2.5px;
+  }
+
+  p.stats {
+    font-size: clamp(1.4rem, 6vw, 1.65rem);
+    font-weight: 450;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .stats-item a {
+    color: #6290fc;
+  }
+
+  .stats-item > span {
+    color: #6290fc;
+  }
+
+  .separator {
+    margin-left: 0.75rem;
+    margin-right: 0.75rem;
+  }
+
   .wave-container {
-    max-width: 450px;
-    height: 150px;
+    margin-top: 2.5rem;
+    margin-bottom: 2.5rem;
+    max-width: 100%;
+    min-height: 150px;
     background-color: var(--bg-color);
     display: flex;
     align-items: center;
     justify-content: space-around;
-    padding: 20px;
   }
+
   .wave-bar {
-    --bar-color: #004bfa;
-    width: 4px;
-    background-color: var(--bar-color);
-    filter: drop-shadow(0 0 1rem var(--bar-color));
+    width: 3px;
+    background-color: var(--wave-blue);
+    filter: drop-shadow(0 0 0.25rem var(--wave-blue));
     border-radius: 2px;
     transition: height 0.1s ease;
   }
